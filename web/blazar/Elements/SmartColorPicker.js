643157b2ace2d3e5d24599388a10917df4d1f899
Vladimir Munteanu <vlmu@trackman.com>
2025-10-10 03:31:45 +0300

    fix

--- web/blazar/Elements/SmartColorPicker.js
+++ web/blazar/Elements/SmartColorPicker.js
@@ -1,6 +1,6 @@
 import { colorUtil } from './colorUtil.js'
 
-export class ElSmartPicker{
+export class ElSmartPicker {
   constructor(id, onChange = null) {
     this.root = document.getElementById(id);
     this.label = this.root.querySelector('label');
@@ -19,16 +19,34 @@ export class ElSmartPicker{
   }
 
   updateLabel() {
-    const hex = this.color.value;
-    this.label.textContent = colorUtil.getNearestColorName(hex);
+    const name = this.#formatName(colorUtil.getNearestColorName(this.color.value));
+    this.label.textContent = name;
+    this.#autoFitText(this.label, this.root.offsetWidth - 10);
   }
-  
+
   setColor(hex) {
     this.color.value = hex;
     this.updateLabel();
   }
 
   setColorRandom() {
-    this.setColor( colorUtil.getRandom() );
+    this.setColor(colorUtil.getRandom());
+  }
+
+  //.......................................PRIVATE
+
+  #formatName(name) {
+    // split camelCase into words
+    const spaced = name.replace(/([a-z])([A-Z])/g, '$1 $2');
+    // capitalize each word
+    return spaced.replace(/\b\w/g, c => c.toUpperCase());
+  }
+
+  #autoFitText(el, maxWidth) {
+    let size = parseFloat(getComputedStyle(el).fontSize);
+    while (el.scrollWidth > maxWidth && size > 6) {
+      size -= 0.5;
+      el.style.fontSize = size + 'px';
+    }
   }
 }
