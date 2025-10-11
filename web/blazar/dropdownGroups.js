542c413d6d4254098885bc7484056fa516efa100
Vladimir Munteanu <vlmu@trackman.com>
2025-10-11 20:01:57 +0300

    fix

--- web/blazar/dropdownGroups.js
+++ web/blazar/dropdownGroups.js
@@ -9,15 +9,17 @@ export class DropdownGroup {
     const numCnt = this.content.children.length;
     if (numOpt !== numCnt) throw new Error(`count options (${numOpt}) != count content items (${numCnt})`);
     // handlers
-    this.selector.addEventListener('change', x => this.#setOptionValue(x));
+    this.selector.addEventListener('change', () => this.#setOptionValue(this.selector.value));
     // init visuals
     this.#setOptionValue(this.selector.value);
+    console.log('dropdown created ' + idSelector);
   }
 
   #setOptionValue(value) {
     const idx = parseInt(value, 10) - 1;
-    for (let i = 0; i < this.content.length; i++)
-      this.content[i].style.display = i === idx ? 'flex' : 'none';
+    for (let i = 0; i < this.content.children.length; i++) {
+      this.content.children[i].style.display = i === idx ? 'flex' : 'none';
+    }
   }
 
 }
